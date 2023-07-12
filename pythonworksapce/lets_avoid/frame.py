94195ea4f2fa1b467e67e5c842abfba4ceaab375
mmj5548 <112236139+mmj5548@users.noreply.github.com>
2023-07-12 15:00:51 +0900

    Update frame.py
    V3

--- pythonworksapce/lets_avoid/frame.py
+++ pythonworksapce/lets_avoid/frame.py
@@ -25,9 +25,11 @@ character_height = character_size[1]
 character_xpos = (screen_width/2) - (character_width/2)
 character_ypos = screen_height -  character_height
 
+#이동위치
+character_speed = 10
+to_x = 0
+
 #이동좌표
-x = 0
-y = 0
 
 
 running = True
@@ -38,12 +40,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    #게임캐릭터 위치정의
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                to_x = -character_speed
+            elif event.key == pygame.K_RIGHT:
+                to_x = character_speed
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                to_x = 0
+
+    #게임캐릭터 위치정의
+    character_xpos += to_x
+
+    
     #충돌처리
 
-    #배경그리기
+    #화면그리기
     screen.blit(background, (0,0))
 
     screen.blit(character, (character_xpos,character_ypos))
