f33b01013c11b398d89e0aa7f57b5d415d8f6115
mmj5548 <112236139+mmj5548@users.noreply.github.com>
2023-08-30 19:04:02 +0900

    Add files via upload

--- real_game/frame.py
+++ real_game/frame.py
@@ -21,22 +21,24 @@ player = pygame.transform.scale(player, (128,128))
 player_X = screen_width/2
 player_Y = screen_height - 128
 fire = False
+
+global i, On, ballXY
+
 ball_Y = 5
 ballXY = []
-global i
 i = 0
-
 j = 0
 On = False
 
 bg = pygame.image.load("images/bg.jpg")
 bg = pygame.transform.scale(bg, (720,960))
 
+ball = pygame.image.load("images/ball.png")
+ball = pygame.transform.scale(ball, (128,64))
+ball = pygame.transform.rotate(ball, -90)
+
 def ball_fire():
-    global i
-    ball = pygame.image.load("images/ball.png")
-    ball = pygame.transform.scale(ball, (128,64))
-    ball = pygame.transform.rotate(ball, -90)
+    global i, On, ballXY
     ballXY.append([player_X + 32,player_Y - 128])
     On = True
     i += 1
@@ -83,10 +85,11 @@ while running:
     #미사일 발사
     if fire == True:
         ball_fire()
+        fire = False
 
     if On == True:
-        for j in range(i+1):
-            ballXY[j][1] = -100
+        for j in range(0, i):
+            ballXY[j][1] -= 10
             screen.blit(ball, (ballXY[j][0], ballXY[j][1]))
 
 
